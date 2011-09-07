ab76b649e08fdbf05bb109f8c45cfb98860545e3
Simon Käser <skaeser@raptus.com>
2011-09-07 09:43:13 +0000

    Made backlink viewlet aware of INavigationRoot

--- raptus/backlink/browser/back.py
+++ raptus/backlink/browser/back.py
@@ -4,6 +4,7 @@ from zope.component import getMultiAdapter
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.navigation.interfaces import INavigationRoot
 from plone.memoize.instance import memoize
 
 from Products.CMFCore.interfaces import IContentish
@@ -21,10 +22,11 @@ class Viewlet(ViewletBase):
     @memoize
     def parent(self):
         context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
-        parent = aq_parent(aq_inner(self.context))
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
         if context_state.is_default_page():
             parent = aq_parent(parent)
-        if not IContentish.providedBy(parent) and not IPloneSiteRoot.providedBy(parent):
+        if INavigationRoot.providedBy(context) or (not IContentish.providedBy(parent) and not IPloneSiteRoot.providedBy(parent)):
             return
         return {'url': parent.absolute_url(),
                 'title': _(u'back to ${parent}', mapping=dict(parent=safe_unicode(parent.Title())))}

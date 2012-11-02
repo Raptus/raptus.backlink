01ed43ae8b2f269a4ef9496d3237295db343af75
Jonas Wagner <jwagner@raptus.com>
2012-11-02 15:59:14 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b4'
 setup(name='raptus.backlink',
       version=version,
       description="Provides a link back to the parent object",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.backlink',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.backlink',
+      url='https://github.com/Raptus/raptus.backlink',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus'],
